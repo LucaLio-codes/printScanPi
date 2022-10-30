5c1fbc98f5a9b030f4bbcd9c64d41e1417ef53f1
luca <luca.liontos@stud.tu-darmstadt.de>
2022-10-30 14:56:28 +0100

    display simple button

--- src/views/ScannerView.vue
+++ src/views/ScannerView.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
-    <v-button text="test" />
+    <v-btn @click="debug"> test </v-btn>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
   name: "ScannerView",
+  methods: {
+    debug() {
+      console.log("debug");
+    },
+  },
 });
 </script>
