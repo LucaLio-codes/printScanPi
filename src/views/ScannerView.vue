dfa15f2cda3839b9205a5f64bb95911293b74216
luca <luca.liontos@stud.tu-darmstadt.de>
2022-10-30 14:34:29 +0100

    test for setup

---
+++ src/views/ScannerView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <v-button text="test" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "ScannerView",
+});
+</script>
